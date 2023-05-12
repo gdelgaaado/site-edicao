308bc3dc50f662688670e7f1b131ee969353ec17
Gabriel Delgado <gdelgadodev@gmail.com>
2023-05-12 03:10:44 -0300

    responsividade

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ function clickMenu(){
     else{
         itens.style.display = "block"
     }
-    
+}   
 
+function mudouTamanho(){
+
+    if(window.innerWidth >= 768){
+      itens.style.display = "block"  
     }
+
+    else{
+        itens.style.display = "none"
+    }
+}
